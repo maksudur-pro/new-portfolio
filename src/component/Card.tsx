c124fe9e211f59649dec5285cdf5e8799dbb46ad
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-11-01 15:41:21 +0600

    added project & skill

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -4,19 +4,20 @@ import joy from "../assets/joy.webp";
 
 const Card = () => {
   return (
-    <div className="bg-white w-full font-Montserrat text-left overflow-hidden shadow-lg rounded-lg mb-6 ">
+    <div className="bg-white w-full font-Montserrat text-left overflow-hidden shadow-lg rounded-lg mb-6 p-6">
       <figure>
         <img
           src={joy}
+          width={318}
+          height={395}
           loading="eager"
           fetchPriority="high"
           alt="Maksudur Rahman Joy"
-          height={355}
-          className="rounded-[30px] w-full  p-6  object-cover blur-sm"
-          onLoad={(e) => e.currentTarget.classList.remove("blur-sm")}
+          className=" rounded-md w-full h-auto object-cover "
         />
-        <figcaption className="p-4">
-          <h2 className="font-semibold text-[#4F4F4F] text-2xl mt-2 sm:text-xl">
+
+        <figcaption className="mt-4 ">
+          <h2 className="font-semibold text-[#4F4F4F] text-2xl sm:text-xl">
             Maksudur Rahman Joy
           </h2>
           <p className="text-lg font-medium text-[#828282]">
@@ -24,7 +25,7 @@ const Card = () => {
           </p>
         </figcaption>
       </figure>
-      <div className="p-4">
+      <div className="my-4 ">
         <a
           href="mailto:maksudurjoy7@gmail.com"
           className=" text-lg flex gap-2  font-medium items-center text-blue-800">
@@ -43,7 +44,7 @@ const Card = () => {
         </a>
       </div>
 
-      <p className="text-lg font-medium text-[#828282] p-4">
+      <p className="text-lg font-medium text-[#828282] mb-4 ">
         I&apos;m a driven developer passionate about creating exceptional UI
         applications through continuous learning and innovation.
       </p>
