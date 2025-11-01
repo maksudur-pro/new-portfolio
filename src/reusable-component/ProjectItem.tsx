051bc1539a75b044810b95c0982263423ca72974
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-11-01 16:03:35 +0600

    added project & skill

--- src/reusable-component/ProjectItem.tsx
+++ src/reusable-component/ProjectItem.tsx
@@ -17,18 +17,21 @@ type Props = {
 const ProjectItem: React.FC<Props> = ({ project }) => {
   return (
     <div
-      className="bg-white font-Montserrat p-4 m-4 shadow-lg rounded-lg lg:flex md:flex"
+      className="bg-white font-Montserrat p-4 m-4 shadow-lg rounded-lg flex flex-col lg:flex-row gap-4"
       key={project.title}>
-      <div className="lg:w-1/3 md:w-1/3 mr-4">
-        <img
-          src={project.image}
-          loading="lazy"
-          alt={project.title}
-          className="rounded-lg w-full h-[300px] object-cover object-top duration-[4000ms] hover:object-bottom cursor-pointer blur-sm"
-          onLoad={(e) => e.currentTarget.classList.remove("blur-sm")}
-        />
+      <div className="lg:w-1/3 md:w-1/3">
+        <div className="w-full aspect-square overflow-hidden">
+          <img
+            src={project.image}
+            loading="lazy"
+            width={400}
+            height={300}
+            alt={project.title}
+            className="rounded-lg w-full h-full object-cover  object-top transition-[object-position] duration-[2000ms] ease-in-out hover:object-bottom cursor-pointer "
+          />
+        </div>
       </div>
-      <div className="lg:w-1/2 md:w-1/2 ml-2">
+      <div className="lg:w-1/2 md:w-1/2 ">
         <h3 className="text-[#333] text-2xl font-medium my-4">
           {project.title}
         </h3>
